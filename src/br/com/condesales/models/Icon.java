970d4d293f27bd1fe872ba87d52cb7adeb0cb91b
Felipe Conde <condesales@gmail.com>
2012-06-29 14:58:44 -0300

    new functionalities

---
+++ src/br/com/condesales/models/Icon.java
@@ -0,0 +1,36 @@
+package br.com.condesales.models;
+
+
+public class Icon {
+
+	private String prefix;
+	
+	private String name;
+	
+	private int[] sizes;
+
+	public String getPrefix() {
+		return prefix;
+	}
+
+	public void setPrefix(String prefix) {
+		this.prefix = prefix;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int[] getSizes() {
+		return sizes;
+	}
+
+	public void setSizes(int[] sizes) {
+		this.sizes = sizes;
+	}
+	
+}
---
+++ src/br/com/condesales/models/Score.java
@@ -0,0 +1,11 @@
+package br.com.condesales.models;
+
+import java.util.ArrayList;
+
+public class Score {
+
+	private int total;
+
+	private ArrayList<ScoreItem> scores;
+
+}
